585665f065ed2760e4e03c571a409e38e99d35e0
Evan Finkelstein <evanfinkelstein91@gmail.com>
2020-11-10 09:48:41 -0800

    f

--- data/munge-yelp.js
+++ data/munge-yelp.js
@@ -1,11 +1,11 @@
 function mungeYelp(yelpData) {
-    let returnArray = yelpData.data.map(yelp => {
+    let returnArray = yelpData.businesses.map(yelp => {
         return {
-            name: yelp.businesses.name,
-            image_url: yelp.businesses.image_url,
-            price: yelp.businesses.price,
-            rating: yelp.businesses.rating,
-            url: yelp.businesses.url,
+            name: yelp.name,
+            image_url: yelp.image_url,
+            price: yelp.price,
+            rating: yelp.rating,
+            url: yelp.url,
 
         }
     })
